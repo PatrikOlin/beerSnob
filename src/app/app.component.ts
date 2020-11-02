d653d375f1cafe5ef10c1fb94d29af0e2f17bddf
Patrik Olin <enmejl@gmail.com>
2020-11-02 22:30:26 +0100

    hur fan fungerar callbacks?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'untappdbolaget';
+  beerName = 'testbärs';
+
+  constructor() {}
+                      
+  ngOnInit() {
+    this.getBeerFromPage(this._getName)
+  }
+                      
+  getBeerFromPage(callback) {
+    let name = 'kuken'; 
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      const url = tabs[0].url;
+      const urls = url.split('/');
+      name = urls[urls.length - 2];    
+      callback(name)
+    });
+  }
+
+  _getName(name){
+    alert(name)
+    this.beerName = name;       
+  }
 }
+                      
+                      
