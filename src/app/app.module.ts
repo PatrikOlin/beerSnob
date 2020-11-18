a46e52507778b6f37e1c4b885ecc32edbc4eb375
patrik olin <enmejl@gmail.com>
2020-11-18 18:26:06 +0100

    added new topbar-component and applied styling to login page. also changed text to swedish, but you'll never get my commit messages in swedish!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, TopBarComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,15 @@
+///<reference types="chrome"/>
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css'],
+})
+export class TopBarComponent {
+  title = 'Brygd';
+  @Input() username = '';
+
+  constructor() {}
+
+}
